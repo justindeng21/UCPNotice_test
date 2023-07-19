60f8d09743c7f950234a1793804490c8763f2d12
justindeng21 <justin.n.deng@gmail.com>
2023-07-19 12:30:41 -0600

    auto deploy

--- server.js
+++ server.js
@@ -51,6 +51,10 @@ app.get('/OCD-29010/example-of-bug', function(req,res){
     res.sendFile('views/example-of-bug.html',{root: __dirname })
 })
 
+app.get('/OCD-29010/example-of-bug-2', function(req,res){
+    res.sendFile('views/example-of-bug-2.html',{root: __dirname })
+})
+
 app.get('/OCD-29010/example-of-working-tag', function(req,res){
     res.sendFile('views/example-of-working-tag.html',{root: __dirname })
 })
