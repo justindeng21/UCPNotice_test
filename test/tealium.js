2dcf49bdd7d5783c4968cd240b03792e51ce2197
Justin Deng <justin.deng@crownpeak.com>
2023-10-13 12:41:03 -0600

    auto deploy

--- test/tealium.js
+++ test/tealium.js
@@ -1,12 +1,12 @@
 var utag_data = {
-    consent_categories : "", // 
+    consent_categories : [], // 
     event_name : "", // Tealium variable to identify unique events eg. social_share, user_login, etc.
     tealium_event : "", // Identifies the unique names of tracked events.
     page_type : "", // Tealium variable to identify page types/templates eg. home, category, search, product, etc.
     test_10_13 : "" // 
 }
 
-
+utag_data.consent_categories.push('analytics')
 
 
 console.log(utag_data.test_10_13)
