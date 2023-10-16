fc9d1d7c589dda164bf13374b1d0cbc14a8be7f3
Justin Deng <justin.deng@crownpeak.com>
2023-10-16 10:07:48 -0600

    auto deploy

--- test/tealium.js
+++ test/tealium.js
@@ -1,5 +1,5 @@
 var utag_data = {
-    consent_categories : [], // 
+    consent_categories : "", // 
     event_name : "", // Tealium variable to identify unique events eg. social_share, user_login, etc.
     tealium_event : "", // Identifies the unique names of tracked events.
     page_type : "", // Tealium variable to identify page types/templates eg. home, category, search, product, etc.
