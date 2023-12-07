6f52dad41ecd2cea648b8e0800d13e71c6d71fdd
justindeng21 <justin.n.deng@gmail.com>
2023-12-06 22:33:52 -0700

    auto deploy

--- js/evidon-sitenotice-tag-ocd-29618.js
+++ js/evidon-sitenotice-tag-ocd-29618.js
@@ -3383,6 +3383,12 @@ window.evidon.uriEncodeComponentCookie = true; // Can be defined in ucp tag
     ;
     aH.prototype._writeCookie = function(aQ, aS, aP, aR, aO) {
 
+        console.log('aQ=',aQ)
+        console.log('aS=',aS)
+        console.log('aP=',aP)
+        console.log('aR=',aR)
+        console.log('aO=',aO)
+
 
         if (window.evidon.uriEncodeCookie) {
             aS = encodeURI(aS)
--- js/unit-test.js
+++ js/unit-test.js
@@ -1,11 +1,3 @@
-const path = '/';
-const domain = null;
-const date = new Date(1970,1,1)
-
-
-
-
-
 function getConsentObject(date){
 
     const jsonDate = date.toISOString()
@@ -15,8 +7,20 @@ function getConsentObject(date){
         consent_type:1
     }
     
-    return JSON.stringify(consentDataObject)
+    return consentDataObject
 }
 
 
-window.evidon.notice._writeCookie('evidonConsentCookie_test',getConsentObject(date),  date.toUTCString(), path, domain )
+
+
+const cookieName = 'evidonConsentCookie_test'
+const date = new Date(1970,1,1)
+const value = getConsentObject(date)
+const path = '/';
+const domain = 'dg-ucp.herokuapp.com';
+
+
+window.evidon.notice._writeCookie(cookieName, JSON.stringify(getConsentObject(date)),  date.toUTCString(), path, domain )
+const parsedCookie = window.evidon.notice._readCookies()
+const trueValue = [{name:cookieName,value:getConsentObject(date)}]
+console.assert(parsedCookie === trueValue,'Failed')
